e82e08dc69dfba85abd24ebb6c608b33fa496f10
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-04-26 18:33:00 +0400

    12

--- amazon-services-test-app-fsdm-oct-24-4-az/Services/IStorageService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Amazon_Services_Test_App_FSDM_Oct_24_4_az.Services;
-
-public interface IStorageService
-{
-    Task<string> UploadFileAsync(IFormFile? file);
-    Task DeleteFileAsync(string fileUrl);
-
-}
--- amazon-services-test-app-fsdm-oct-24-4-az/productSite/src/config.js
+++
@@ -1,2 +0,0 @@
-export const API_BASE = 'http://localhost:5048';
-export const PLACEHOLDER_IMAGE = 'https://placehold.co/400x240?text=No+Image';
